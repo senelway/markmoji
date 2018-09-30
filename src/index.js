be6960840a3ae444a411e483d9ef940e08f880a0
__strelkov <stkdmitry@ya.ru>
2018-10-01 02:20:47 +0300

    informer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const root = document.body;
 
 const toLowerCase = e => e.toLowerCase();
 
-const copyTextToClipboard = text => {
+const copyTextToClipboard = text => new Promise((resolve) => {
   const copyFrom = document.createElement("textarea");
   copyFrom.textContent = text;
   document.body.appendChild(copyFrom);
@@ -15,12 +15,27 @@ const copyTextToClipboard = text => {
   document.execCommand('copy');
   copyFrom.blur();
   document.body.removeChild(copyFrom);
-}
+  resolve();
+});
 
 const markmoji = {
   search: '',
-  onCopy: copyTextToClipboard,
+  showInfo: false,
+
+  onToggleInformer: () => markmoji.showInfo = !markmoji.showInfo,
+
+  onCopy: text => {
+    markmoji.onToggleInformer();
+    copyTextToClipboard(text).then(() => {
+      setTimeout(() => {
+        markmoji.onToggleInformer();
+        m.redraw(markmoji);
+      }, 3000)
+    });
+  },
+
   onChange: ({ currentTarget }) => markmoji.search = currentTarget.value,
+
   onFiltrate: data => {
     const keys = Object.keys(data);
     if (markmoji.search) {
@@ -28,9 +43,11 @@ const markmoji = {
     }
     return keys;
   },
+
   view: function () {
     return [
       m('input', { onchange: markmoji.onChange, placeholder: 'filtrate emoji' }),
+      m('div', { class: `b-alert js-alert is-success ${markmoji.showInfo ? 'is-show' : ''}` }, 'copied'),
       m('div', { class: 'b-flex' }, [
         markmoji.onFiltrate(json).length
           ? markmoji.onFiltrate(json).map(e => (
@@ -39,7 +56,7 @@ const markmoji = {
               m('code', json[e])
             ])
           ))
-          : m('p', { class: 'b-alert' }, 'emoji not found')
+          : m('p', { class: 'b-error' }, 'emoji not found')
       ])
     ]
   }
